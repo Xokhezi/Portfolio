d8a25bf3231b56fd6fe3141369340a97b8df4460
Xokhezi <110103053+Xokhezi@users.noreply.github.com>
2023-12-11 10:57:15 +0100

    scroll animations

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const textElement = document.getElementById('changingText');
-    const textArray = ["Web Developer", "Content Creator"];
-    var currentIndex = 0;
+  const the_animation = document.querySelectorAll(['.fadein','.slidein',".popup"])
+  const textElement = document.getElementById('changingText');
+  const textArray = ["Web Developer", "Content Creator"];
+  var currentIndex = 0;
 
-    const sections = document.querySelectorAll("section");
-    const navLinks = document.querySelectorAll("nav ul li a");
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll("nav ul li a");
 
+  const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add('scroll-animation')
+          }
+              else {
+                  entry.target.classList.remove('scroll-animation')
+              }
+          
+      })
+  },
+     { threshold: 0.3
+     });
+    for (let i = 0; i < the_animation.length; i++) {
+     const elements = the_animation[i];
+  
+      observer.observe(elements);
+    } 
+
+    
     function changeText() {
       textElement.textContent = textArray[currentIndex];
       currentIndex = (currentIndex + 1) % textArray.length;
